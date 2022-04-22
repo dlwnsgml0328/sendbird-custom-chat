411a25753c0233984e7a5e944373982a7b6fb152
dlwnsgml0328 <junhee@eazel.net>
2022-04-22 16:38:11 +0900

    add: add files https://github.com/sendbird/sendbird-uikit-react/tree/ec74b7e1d735094a436015b7e89e10ae9232711c/samples/groupchannel/src

---
+++ react/react-app-custom-v3-react-17/groupchannel/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SendBirdProvider from '@sendbird/uikit-react/SendbirdProvider';
+import '@sendbird/uikit-react/dist/index.css';
+
+import CustomizedApp from './CustomizedApp';
+import './styles.css';
+
+import { APP_ID, USER_ID, NICKNAME } from './const';
+
+export default function App() {
+  if (!APP_ID) {
+    return <p>Set APP_ID in const.js</p>;
+  }
+  return (
+    <SendBirdProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
+      <CustomizedApp />
+    </SendBirdProvider>
+  );
+}
---
+++ react/react-app-custom-v3-react-17/groupchannel/src/const.js
@@ -0,0 +1,9 @@
+export const APP_ID = 'ABD4B8EB-4479-4478-9C51-D41B009C6780';
+export const USER_ID = 'junhee';
+export const NICKNAME = 'junhee';
+
+export default {
+  APP_ID,
+  USER_ID,
+  NICKNAME,
+};
